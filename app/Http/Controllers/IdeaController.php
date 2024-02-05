28ecaa92e30fc354b970f66de3360f577cc2413e
jorgequintero <jquintedori@gmail.com>
2024-02-05 13:55:50 -0500

    add search behavior

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -10,15 +10,32 @@ class IdeaController extends Controller
     public function show(Idea $idea){
         return view('ideas.show',compact('idea'));
     }
+
+    public function edit(Idea $idea){
+        $editing=true;
+        return view('ideas.show',compact('idea', 'editing'));
+    }
+    
+    public function update(Idea $idea){
+        request()->validate([
+            'content' =>'required|min:3|max:240',
+        ]);
+
+        $idea->content= request()->get('content','');
+        $idea->save();
+
+        return redirect()->route('ideas.show', $idea->id)->with('success', 'Idea was updated successfully!');
+    }
+
     public function store(){
 
         request()->validate([
-            'idea' =>'required|min:3|max:240',
+            'content' =>'required|min:3|max:240',
         ]);
 
         $idea = Idea::create(
             [
-            'content' => request()->get('idea',''),
+            'content' => request()->get('content',''),
             ]
     );
     return redirect()->route('dashboard')->with('success', 'Idea was created successfully!');
--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -1,10 +1,10 @@
 <h4> Share yours ideas </h4>
 <div class="row">
-    <form action="{{route('ideas.create')}}" method="POST">
+    <form action="{{route('ideas.store')}}" method="POST">
         @csrf
         <div class="mb-3">
-            <textarea name="idea" class="form-control" id="idea" rows="3"></textarea>
-            @error('idea')
+            <textarea name="content" class="form-control" id="content" rows="3"></textarea>
+            @error('content')
                 <span class="d-block fs-6 text-danger mt-2">{{$message}}</span>
             @enderror
         </div>
